7f7fb533babc0b783c20e82527ddc7f0ebfbb629
Alexei Nunez <alexei.nunez@bazaarvoice.com>
2018-05-22 11:59:09 -0500

    make compliant with melpa package-lint

--- ob-mermaid.el
+++ ob-mermaid.el
@@ -1,4 +1,34 @@
+;;; ob-mermaid.el --- org-babel support for mermaid evaluation
 
+;; Copyright (C) 2018 Alexei Nunez
+
+;; Author: Alexei Nunez <alexeirnunez@gmail.com>
+;; URL: https://github.com/arnm/ob-mermaid
+;; Keywords: lisp
+;; Version: 0
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Org-Babel support for evaluating mermaid diagrams.
+
+;;; Requirements:
+
+;; mermaid.cli | https://github.com/mermaidjs/mermaid.cli
+
+;;; Code:
 (require 'ob)
 (require 'ob-eval)
 
@@ -6,7 +36,7 @@
   '((:results . "file") (:exports . "results"))
   "Default arguments for evaluatiing a mermaid source block.")
 
-(defcustom org-mermaid-path nil
+(defcustom ob-mermaid-cli-path nil
   "Path to mermaid.cli executable."
   :group 'org-babel
   :type 'string)
@@ -15,16 +45,19 @@
   (let* ((out-file (or (cdr (assoc :file params))
                        (error "mermaid requires a \":file\" header argument")))
          (temp-file (org-babel-temp-file "mermaid-"))
-         (cmd (if (not org-mermaid-path)
-                  (error "`org-mermaid-path' is not set")
-                (concat (shell-quote-argument (expand-file-name org-mermaid-path))
+         (cmd (if (not ob-mermaid-cli-path)
+                  (error "`ob-mermaid-cli-path' is not set")
+                (concat (shell-quote-argument (expand-file-name ob-mermaid-cli-path))
                         " -i " (org-babel-process-file-name temp-file)
                         " -o " (org-babel-process-file-name out-file)))))
-    (unless (file-exists-p org-mermaid-path)
-      (error "could not find mermaid.cli executable at %s" org-mermaid-path))
+    (unless (file-exists-p ob-mermaid-cli-path)
+      (error "could not find mermaid.cli executable at %s" ob-mermaid-cli-path))
     (with-temp-file temp-file (insert body))
     (message "%s" cmd)
     (org-babel-eval cmd "")
     nil))
 
 (provide 'ob-mermaid)
+
+
+;;; ob-mermaid.el ends here
